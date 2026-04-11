af5458c5b40e5bac3d4d62e2e9ac1d5544ac94a0
Lockdown08 <hbktitus4@gmail.com>
2026-04-11 19:13:23 +0200

    Main file added

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Login userLogin = new Login();
+        
+        System.out.println("Rgistration");
+        
+        System.out.print("Enter First Name: ");
+        String fName = scanner.nextLine();
+        
+        System.out.print("Enter Last Name: ");
+        String lName = scanner.nextLine();
+        
+        System.out.print("Enter a Username with max 5 chars, must contain '_': ");
+        String user = scanner.nextLine();
+        
+        System.out.print("Enter a Password.. min of 8 chars, 1 uppercase, 1 number, 1 special:  ");
+        String pass = scanner.nextLine();
+        
+        System.out.print("Enter an SA NO include the international code +27: ");
+        String cell = scanner.nextLine();
+        
+        String registrationResult = userLogin.registerUser(user, pass, fName, lName, cell);
+        System.out.println("\n" + registrationResult);
+        
+        
+
+
+        
